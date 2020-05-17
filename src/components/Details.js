b439f5cb7ca1c8f5fc60d868d6d57a819025c6d8
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2020-05-17 21:02:09 +1200

    add check if user exists in firestore database; if exists, showprofile details and if not show an error message

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,18 +1,28 @@
 import React from "react"
+import DetailsNotFound from "./DetailsNotFound"
 import { Redirect } from "@reach/router"
 import { connect } from "react-redux"
 import { compose } from "redux"
 import { firestoreConnect } from "react-redux-firebase"
 
-const Details = ({ location, sessionId, username }) => {
-  return (
-    <div>
-      {console.log(location)}
-      test {sessionId}
-    </div>
-  )
-}
+const Details = ({ location, sessionId, cards, requested }) => {
+  const getUserDetails = () => {
+    if (cards && requested) {
+      return cards.length === 0 ? false : true
+    }
+  }
 
+  if (getUserDetails()) {
+    return (
+      <div>
+        {console.log(location)}
+        test {sessionId}
+      </div>
+    )
+  } else {
+    return <DetailsNotFound />
+  }
+}
 
 const mapStateToProps = ({ firestore }) => {
   return {
@@ -28,9 +38,8 @@ export default compose(
       {
         collection: "cards",
         where: ["session_id", "==", props.sessionId],
-        storeAs: "currentCardDetail"
+        storeAs: "currentCardDetail",
       },
-
     ]
   })
 )(Details)
---
+++ src/components/DetailsNotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DetailsNotFound = () => {
+    return (
+        <div>
+            user not found
+        </div>
+    );
+};
+
+export default DetailsNotFound;
