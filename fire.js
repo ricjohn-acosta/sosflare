e083af2e63f0e14b7b56468b33a9a60b2547cd9b
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2020-05-24 22:49:23 +1200

    refactor fire.js

--- fire.js
+++ fire.js
@@ -3,10 +3,9 @@ import "firebase/auth"
 import "firebase/firestore"
 
 const environment = process.env.GCLOUD_PROJECT
-let firebaseConfig;
 
-if(environment === "sos-flare") {
-   firebaseConfig = {
+if (environment === "sos-flare") {
+  const firebaseConfig = {
     apiKey: "AIzaSyAB1i-ulKCLaF_0niwmXw6ciypsPCsdCEw",
     authDomain: "sos-flare.firebaseapp.com",
     databaseURL: "https://sos-flare.firebaseio.com",
@@ -16,8 +15,9 @@ if(environment === "sos-flare") {
     appId: "1:8242918205:web:ea5d132abc302eedc5cb54",
     measurementId: "G-N5G3ZTNSCV",
   }
+  firebase.initializeApp(firebaseConfig)
 } else {
-   firebaseConfig = {
+  const firebaseConfig = {
     apiKey: "AIzaSyCLSUUjwdD1WRxi_tCmyNDBwdD8gCYTX4A",
     authDomain: "live-sos-flare.firebaseapp.com",
     databaseURL: "https://live-sos-flare.firebaseio.com",
@@ -25,11 +25,10 @@ if(environment === "sos-flare") {
     storageBucket: "live-sos-flare.appspot.com",
     messagingSenderId: "717986119116",
     appId: "1:717986119116:web:57fdce9405236bd5117169",
-    measurementId: "G-XW0BKKWSCD"
-  };
+    measurementId: "G-XW0BKKWSCD",
+  }
+  firebase.initializeApp(firebaseConfig)
 }
 
-
-firebase.initializeApp(firebaseConfig)
 firebase.firestore()
 export default firebase
