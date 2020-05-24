f46ef8693f027575f2a85e34160bb61d76d60d20
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2020-05-25 00:52:31 +1200

    successfully create dev and prod environments

--- fire.js
+++ fire.js
@@ -2,17 +2,34 @@ import firebase from "firebase/app"
 import "firebase/auth"
 import "firebase/firestore"
 
-const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+console.log("PROJECT ID ", process.env.API_KEY)
+if (process.env.ENVIRONMENT === "development") {
+  const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: "sos-flare",
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID,
+  }
+  
+  firebase.initializeApp(firebaseConfig) 
+} else {
+  const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: "live-sos-flare",
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID,
+  }
+  
+  firebase.initializeApp(firebaseConfig)
 }
 
-firebase.initializeApp(firebaseConfig)
 firebase.firestore()
 export default firebase
