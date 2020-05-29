cc54e8a6c3a64aea3bcf74512b53397d8d63d15d
ricjohn-acosta <ricjohn.genoguin@gmail.com>
2020-05-30 02:09:04 +1200

    change favicon

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,6 +5,6 @@
  */
 
 // You can delete this file if you're not using it
+import "@babel/polyfill"
 import wrapWithProvider from "./src/store/wrap-with-provider"
-import 'babel-polyfill'
 export const wrapRootElement = wrapWithProvider
